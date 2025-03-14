7c9f64f539b864c3a6dd969b1c17e572671518e7
Kaisiq <dahudohu@gmail.com>
2025-03-14 22:57:59 +0200

    Move card quiz card into separate component; add select from shadcn; install pdfjs and html2canvas

--- src/app/dashboard/new/page.tsx
+++ src/app/dashboard/new/page.tsx
@@ -4,13 +4,18 @@ import { LinkButton } from "~/components/ui/linkbutton";
 import { useState } from "react";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
-import { Textarea } from "~/components/ui/textarea";
-import { RadioGroup, RadioGroupItem } from "~/components/ui/radio-group";
-import { Card, CardContent, CardHeader } from "~/components/ui/card";
 import { Plus, Trash2, Save } from "lucide-react";
 import { ReactSortable } from "react-sortablejs";
 import { Swap } from "sortablejs";
 import Sortable from "sortablejs";
+import QuestionCard from "~/components/QuestionCard";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
 
 interface Answer {
   id: number;
@@ -38,6 +43,7 @@ export default function CreateQuiz() {
       ],
     },
   ]);
+  const [quizColumns, setQuizColumns] = useState("grid-cols-2");
 
   const addQuestion = () => {
     const newQuestion: Question = {
@@ -165,88 +171,39 @@ export default function CreateQuiz() {
             placeholder="Enter quiz title"
             required
           />
+          <Select
+            onValueChange={(value) => {
+              setQuizColumns(value);
+            }}
+          >
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="How much columns per row" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="grid-cols-1">1 Column</SelectItem>
+              <SelectItem value="grid-cols-2">2 Columns</SelectItem>
+              <SelectItem value="grid-cols-3">3 Columns</SelectItem>
+            </SelectContent>
+          </Select>
         </div>
         <ReactSortable
           swap
           list={questions}
           setList={setQuestions}
-          className="grid grid-cols-2 items-center gap-5"
+          className={`grid ${quizColumns} items-center gap-5`}
         >
           {questions.map((question, qIndex) => (
-            <Card
-              key={question.id}
-              className="min-h-32 space-y-4 rounded-md border p-2"
-            >
-              <CardHeader className="flex flex-row items-center gap-2 pb-0">
-                <Textarea
-                  className="text-xl font-semibold"
-                  id={`question-${question.id}`}
-                  value={question.text}
-                  onChange={(e) =>
-                    updateQuestionText(question.id, e.target.value)
-                  }
-                  placeholder={`Question ${qIndex + 1}`}
-                  required
-                />
-                <Button
-                  type="button"
-                  variant="ghost"
-                  size="high"
-                  onClick={() => removeQuestion(question.id)}
-                >
-                  <Trash2 className="h-5 w-5" />
-                </Button>
-              </CardHeader>
-              <CardContent className="flex flex-col gap-2">
-                <RadioGroup>
-                  {question.answers.map((answer, aIndex) => (
-                    <div
-                      key={answer.id}
-                      className="flex items-center space-x-2"
-                    >
-                      <RadioGroupItem
-                        value={answer.id.toString()}
-                        id={`q${question.id}-a${answer.id}`}
-                        checked={answer.isCorrect}
-                        onClick={() =>
-                          toggleCorrectAnswer(question.id, answer.id)
-                        }
-                      />
-                      <Input
-                        value={answer.text}
-                        onChange={(e) =>
-                          updateAnswerText(
-                            question.id,
-                            answer.id,
-                            e.target.value,
-                          )
-                        }
-                        placeholder={`Answer ${aIndex + 1}`}
-                        required
-                        className="flex-grow"
-                      />
-                      {question.answers.length > 1 && (
-                        <Button
-                          type="button"
-                          variant="ghost"
-                          size="icon"
-                          onClick={() => removeAnswer(question.id, answer.id)}
-                        >
-                          <Trash2 className="h-4 w-4" />
-                        </Button>
-                      )}
-                    </div>
-                  ))}
-                </RadioGroup>
-                <Button
-                  type="button"
-                  variant="outline"
-                  onClick={() => addAnswer(question.id)}
-                >
-                  <Plus /> Add Answer
-                </Button>
-              </CardContent>
-            </Card>
+            <QuestionCard
+              key={qIndex}
+              question={question}
+              qIndex={qIndex}
+              updateAnswerText={updateAnswerText}
+              addAnswer={addAnswer}
+              updateQuestionText={updateQuestionText}
+              removeAnswer={removeAnswer}
+              removeQuestion={removeQuestion}
+              toggleCorrectAnswer={toggleCorrectAnswer}
+            />
           ))}
           <Button
             className="mx-auto my-[8.5rem] w-1/2 items-center"
