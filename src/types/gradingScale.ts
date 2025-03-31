0ab8af60540bb9417c05cccb297a236d6d4f5cc8
Kaisiq <dahudohu@gmail.com>
2025-03-31 19:18:10 +0300

    Merge types into one file and add Dictionary type

--- src/types/gradingScale.ts
+++
@@ -1,5 +0,0 @@
-export type GradingScale = {
-  minScore: number;
-  maxScore: number;
-  grade: string;
-}[];
--- src/types/question.ts
+++
@@ -1,7 +0,0 @@
-import type { Answer } from "./answer";
-
-export interface Question {
-  id: number;
-  text: string;
-  answers: Answer[];
-}
